2e37dcda14810150dc172a2794fa005587a07c02
Mirasaki <pcr.hillebrand1998@gmail.com>
2023-06-01 12:12:20 +0200

    fix: wrong day/night cycle acceleration calculation

--- src/modules/server-info.js
+++ src/modules/server-info.js
@@ -2,7 +2,7 @@ const { colorResolver, msToHumanReadableTime } = require('../util');
 const colors = require('../../config/colors.json');
 const emojis = require('../../config/emojis.json');
 const { stripIndents } = require('common-tags');
-const { MS_IN_ONE_DAY } = require('../constants');
+const { MS_IN_ONE_MINUTE } = require('../constants');
 
 const resolveFlags = ({ attributes }) => {
   const flags = [];
@@ -19,52 +19,67 @@ const resolveFlags = ({ attributes }) => {
   return flags;
 };
 
+const calculateDayNightTime = (serverTimeAcceleration, serverNightTimeAcceleration) => {
+  const minutesPerHour = 60;
+  // Calculate the duration of a day in minutes
+  const dayDuration = (24 / serverTimeAcceleration) * minutesPerHour;
+  // Calculate the duration of a night in minutes
+  const nightDuration = (dayDuration / serverNightTimeAcceleration);
+
+  return {
+    day: msToHumanReadableTime(dayDuration * MS_IN_ONE_MINUTE),
+    night: msToHumanReadableTime(nightDuration * MS_IN_ONE_MINUTE)
+  };
+};
+
 // eslint-disable-next-line sonarjs/cognitive-complexity
-const serverInfoOverviewEmbed = (data, flags, guild) => ({
-  color: colorResolver(data.online ? colors.success : colors.error),
-  author: {
-    name: `#${ data.rank } | ` + data.name + (data.map ? ` (${ data.map })` : ''),
-    icon_url: guild.iconURL({ dynamic: true })
-  },
-  description: stripIndents`
-    **IP:** **\`${ data.host?.address ?? '0.0.0.0' }:${ data.host?.gamePort ?? '0000' }\`**
-    **Time:** ${ data.environment?.time ?? 'Unknown' }
-    **Location:** [${ data.geolocation.country?.code }] ${ data.geolocation?.country?.name ?? 'n/a' }
-    **Perspective:** ${ data.environment?.perspectives?.thirdPersonPerspective ? 'First + Third' : 'First Online' }
-    **Time Acceleration:**
-    🌞 ${ msToHumanReadableTime(MS_IN_ONE_DAY / (data.environment?.timeAcceleration?.general ?? 1)) }
-    🌗 ${ msToHumanReadableTime((
-    MS_IN_ONE_DAY / 2
-  ) / (
-    (data.environment?.timeAcceleration?.general ?? 1)
-      + (data.environment?.timeAcceleration?.night ?? 1)
-  )) }
-    **Flags:** \`${ flags.join('`, `') || 'None' }\`
-  `,
-  fields: [
-    {
-      name: 'Players',
-      value: stripIndents`
-        Online: ${ data.status?.players?.online ?? 0 }
-        Slots: ${ data.status?.players?.slots ?? 0 }
-        Queue: ${ data.status?.players?.queue ?? 0 }
-      `,
-      inline: true
+const serverInfoOverviewEmbed = (data, flags, guild) => {
+  const { day, night } = calculateDayNightTime(
+    (data.environment?.timeAcceleration?.general ?? 1),
+    (data.environment?.timeAcceleration?.night ?? 1)
+  );
+  return {
+    color: colorResolver(data.online ? colors.success : colors.error),
+    author: {
+      name: `#${ data.rank } | ` + data.name + (data.map ? ` (${ data.map })` : ''),
+      icon_url: guild.iconURL({ dynamic: true })
     },
-    {
-      name: 'Security',
-      value: stripIndents`
-        VAC: ${ (data.security?.vac ? emojis.success : emojis.error) ?? 'n/a' }
-        BatllEye: ${ (data.security?.battleye ? emojis.success : emojis.error) ?? 'n/a' }
-        Password-Protected: ${ (data.security?.password ? emojis.success : emojis.error) ?? 'n/a' }
-      `,
-      inline: true
-    }
-  ],
-  footer: { text: `DayZ - ${ data.version }` }
-});
+    description: stripIndents`
+      **IP:** **\`${ data.host?.address ?? '0.0.0.0' }:${ data.host?.gamePort ?? '0000' }\`**
+      **Time:** ${ data.environment?.time ?? 'Unknown' }
+      **Location:** [${ data.geolocation.country?.code }] ${ data.geolocation?.country?.name ?? 'n/a' }
+      **Perspective:** ${ data.environment?.perspectives?.thirdPersonPerspective ? 'First + Third' : 'First Online' }
+      **Time Acceleration:**
+      🌞 ${ day }
+      🌗 ${ night }
+      **Flags:** \`${ flags.join('`, `') || 'None' }\`
+    `,
+    fields: [
+      {
+        name: 'Players',
+        value: stripIndents`
+          Online: ${ data.status?.players?.online ?? 0 }
+          Slots: ${ data.status?.players?.slots ?? 0 }
+          Queue: ${ data.status?.players?.queue ?? 0 }
+        `,
+        inline: true
+      },
+      {
+        name: 'Security',
+        value: stripIndents`
+          VAC: ${ (data.security?.vac ? emojis.success : emojis.error) ?? 'n/a' }
+          BatllEye: ${ (data.security?.battleye ? emojis.success : emojis.error) ?? 'n/a' }
+          Password-Protected: ${ (data.security?.password ? emojis.success : emojis.error) ?? 'n/a' }
+        `,
+        inline: true
+      }
+    ],
+    footer: { text: `DayZ - ${ data.version }` }
+  };
+};
 
 module.exports = {
   resolveFlags,
+  calculateDayNightTime,
   serverInfoOverviewEmbed
 };
